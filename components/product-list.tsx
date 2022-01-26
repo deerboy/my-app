5c3f91960718dcba781ac7c3a0be23a43884a775
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2022-01-26 17:27:30 +0900

    add checkout

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,5 +1,12 @@
 import { User } from 'firebase/auth';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { db } from '../lib/firebase';
 
@@ -22,6 +29,41 @@ type Props = {
 
 const ProductList = ({ user }: Props) => {
   const [products, setProducts] = useState<Product[]>();
+
+  const redirectToCheckout = async (priceId: string) => {
+    const collectionRef = collection(
+      db,
+      `customers/${user.uid}/checkout_sessions`
+    );
+    const docRef = await addDoc(collectionRef, {
+      mode: 'payment',
+      billing_address_collection: 'auto',
+      success_url: window.location.origin,
+      cancel_url: window.location.origin,
+      line_items: [
+        {
+          price: priceId,
+          tax_rates: ['txr_1KLKRBHKPHgsaXXh3RsYP1bW'],
+          quantity: 1,
+        },
+      ],
+    });
+
+    onSnapshot(docRef, (snap) => {
+      const { error, url } = snap.data() as {
+        url: string;
+        error: Error;
+      };
+
+      if (error) {
+        alert('エラーが発生しました' + error.message);
+      }
+
+      if (url) {
+        window.location.assign(url);
+      }
+    });
+  };
 
   useEffect(() => {
     const ref = collection(db, 'products');
@@ -57,6 +99,7 @@ const ProductList = ({ user }: Props) => {
             <div key={price.id}>
               {price.description || '通常'} -{' '}
               {price.unit_amount.toLocaleString()}円
+              <button onClick={() => redirectToCheckout(price.id)}>購入</button>
             </div>
           ))}
         </div>
