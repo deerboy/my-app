70634fd30e2046a599155028311c888310444d4a
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2022-01-30 14:26:55 +0900

    add checkout

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,11 +1,19 @@
+import { httpsCallable } from 'firebase/functions';
 import React from 'react';
+import { fns } from '../lib/firebase';
 
 const ProductList = () => {
+  const startCheckout = async () => {
+    const callable = httpsCallable(fns, 'createCheckoutSession');
+    const { data } = await callable();
+    window.location.assign(data as string);
+  };
+
   return (
     <div>
       <h2>チケット</h2>
       <p>1500円</p>
-      <button>購入</button>
+      <button onClick={startCheckout}>購入</button>
     </div>
   );
 };
